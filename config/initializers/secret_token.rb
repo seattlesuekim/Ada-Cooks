fd604339ded56f5eb18ffdc6c0973508068c8223
Sue White <sue@mailworks.org>
2013-12-03 12:38:12 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdaCooks::Application.config.secret_key_base = '8043e3e13758b75a2c78d5f67d73c5645538575bf3ac7374bf3701ff9a549a2f6e98b2e9c3f6534dd54041e9c4d4f2e6d29676c87e6b2ebfda4d178612f82b45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdaCooks::Application.config.session_store :cookie_store, key: '_Ada-Cooks_session'
