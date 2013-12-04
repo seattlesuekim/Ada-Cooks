0978f6903893fd55845004fe40266a35a0885092
Sue White <sue@mailworks.org>
2013-12-03 18:08:48 -0800

    User can add new cookbooks

--- db/migrate/20131203215013_create_recipes.rb
+++ db/migrate/20131203215013_create_recipes.rb
@@ -2,6 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203215105) do
+ActiveRecord::Schema.define(version: 20131204020316) do
 
   create_table "cookbooks", force: true do |t|
     t.string   "recipe"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20131203215105) do
     t.string   "tool"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "name"
   end
 
   create_table "ingredients", force: true do |t|
